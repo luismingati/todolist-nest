979e63c72b3dac4b26247b45bd2f5f9521b80f58
Gabriel Pires <gabrielpires2991@gmail.com>
2024-10-28 20:38:43 -0300

    feat: CRUD /todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [AuthModule, CategoryModule],
+  imports: [AuthModule, CategoryModule, TodoModule],
   controllers: [],
   providers: [],
 })
